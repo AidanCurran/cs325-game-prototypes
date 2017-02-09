70ca1c726215dc512854be175f1f8ec43ff0ab31
AidanCurran <Aidan@Aidans-MBP.byod.gmu.edu>
2017-02-09 02:51:40 -0500

    Finished a bunch of stuff

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -1,65 +1,62 @@
 window.onload = function() 
-{
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
-    
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
+{   
     "use strict";
     
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() 
     {
-        // Load an image and call it 'logo'.
         game.load.image( 'asteroid', 'assets/asteroid.png' );
         game.load.image( 'ground', 'assets/ground.png' );
         game.load.image( 'person', 'assets/person.png' );
     }
     
-    var bouncy;
     var person;
-    var ground;
+    var rocks;
+    var text;
+    var a;
     
     function create() 
     {
-    	//game.physics.startSystem(Phaser.Physics.ARCADE);
-    
-    	person = game.add.sprite(32, game.world.height - 150, 'person');
-    	ground = game.add.sprite(0, game.world.height - 64, 'ground');
-    	ground.body.immovable = true;
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'asteroid' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        // var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        // var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        // text.anchor.setTo( 0.5, 0.0 );
+    	game.physics.startSystem(Phaser.Physics.ARCADE);
+    	game.stage.backgroundColor = '#323a75';
+    	text = game.add.text(125, 180, "YOU DIED! \n      RIP");
+    	text.fill = '#323a75';
+    	text.style.font = '100px Arial';
+	    rocks = game.add.group();
+	    rocks.enableBody = true;
+		rocks.physicsBodyType = Phaser.Physics.ARCADE;
+    	for (var i = 0; i < 250; i++)
+    	{
+        	a = rocks.create(game.world.randomX, -i*30, 'asteroid');
+        	a.name = 'asteroid' + i;
+        	game.physics.enable( a, Phaser.Physics.ARCADE);
+    	}
+    	var ground = game.add.sprite(0, game.world.height - 65, 'ground');
+    	person = game.add.sprite(375, game.world.height - 125, 'person');
+    	game.physics.enable( person, Phaser.Physics.ARCADE);
+    	person.body.collideWorldBounds = true;
+    	
     }
     
     function update() 
     {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
+    	game.physics.arcade.overlap(person, rocks, collisionHandler, null, this);
+    	rocks.y += 5;
+    	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
+    	{
+        	person.x -= 7;
+    	}
+    	else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
+    	{
+        	person.x += 7;
+    	}
     }
+    
+    function collisionHandler (person, rocks) 
+    {
+    	person.kill();
+    	rocks.kill();
+    	text.fill = '#ffffff';
+	}
 };
