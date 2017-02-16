c52c2a77594ad9201ab6871784706c179a61b4ee
AidanCurran <Aidan@Aidans-MacBook-Pro.local>
2017-02-15 21:49:27 -0500

    changes

--- HW2/js/Preloader.js
+++ HW2/js/Preloader.js
@@ -3,7 +3,6 @@ BasicGame.Preloader = function (game) {
 
 	this.background = null;
 	this.preloadBar = null;
-
 	this.ready = false;
 
 };
@@ -26,7 +25,7 @@ BasicGame.Preloader.prototype = {
 		//	As this is just a Project Template I've not provided these assets, swap them for your own.
 		this.load.image('titlePage', 'assets/title.jpg');
 		this.load.atlas('playButton', 'assets/play_button.png', 'assets/play_button.json');
-		this.load.audio('titleMusic', ['assets/Poppers and Prosecco.mp3']);
+		this.load.audio('titleMusic', ['assets/Otis McMusic.mp3']);
 		//	+ lots of other required assets here
         this.load.image( 'logo', 'assets/phaser.png' );
 	},
