fd6f62f9ff19264f9738be4a3bb40d7b170da946
AidanCurran <Aidan@Aidans-MBP.byod.gmu.edu>
2017-04-06 11:53:21 -0400

    hw5

---
+++ HW5/js/MainMenu.js
@@ -0,0 +1,25 @@
+"use strict";
+GameStates.makeMainMenu = function( game, shared ) 
+{
+	var music = null;
+	var playButton = null;   
+    function startGame(pointer) 
+    {
+    	music.stop();
+        game.state.start('Game');
+    }
+    return {
+    
+        create: function () 
+        {
+        	music = this.add.audio('titleMusic');
+			music.play();
+			game.add.sprite(0, 0, 'titlePage');
+			//game.add.sprite(760, 0, 'snob');
+			playButton = this.add.button( 300, 650, 'playButton', startGame, this);
+        },
+        update: function () 
+        {
+        }
+    };
+};
---
+++ HW5/js/main.js
@@ -0,0 +1,13 @@
+"use strict";
+window.onload = function() 
+{
+	var game = new Phaser.Game( 800, 800, Phaser.AUTO, 'game' );
+	var shared = {};
+	game.state.add( 'Boot', GameStates.makeBoot( game ) );
+	game.state.add( 'Preloader', GameStates.makePreloader( game ) );
+	game.state.add( 'MainMenu', GameStates.makeMainMenu( game, shared ) );
+	game.state.add( 'Game', GameStates.makeGame( game, shared ) );
+	game.state.add( 'EndMenu', GameStates.makeMainMenu( game, shared ) );
+	game.state.start('Boot');
+
+};
