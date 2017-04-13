87f5cc096592046abc0390bafe529905238ec662
AidanCurran <Aidan@Aidans-MBP.byod.gmu.edu>
2017-04-13 03:41:09 -0400

    l

--- HW7/js/Preloader.js
+++ HW7/js/Preloader.js
@@ -12,10 +12,10 @@ GameStates.makePreloader = function( game )
             game.load.setPreloadSprite(preloadBar);
             game.load.image('titlePage', 'assets/MainMenu.png');
             game.load.image('diceSpot', 'assets/diceSpot.png');    
- 			game.load.image('redCar', 'assets/redCar.png');
- 			game.load.image('greenCar', 'assets/greenCar.png');
- 			game.load.image('yellowCar', 'assets/yellowCar.png');
- 			game.load.image('blueCar', 'assets/blueCar.png');
+ 			game.load.image('redCar', 'assets/r.png');
+ 			game.load.image('greenCar', 'assets/g.png');
+ 			game.load.image('yellowCar', 'assets/y.png');
+ 			game.load.image('blueCar', 'assets/b.png');
 			game.load.image('playButton', 'assets/playButton.png');
 			game.load.audio('backgroundMusic', ['assets/backgroundMusic.mp3']);
  			game.load.audio('titleMusic', ['assets/carNoise.mp3']);
